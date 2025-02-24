421fed16aeae54b9b697c87a5aa200f82f828d9d
24LP0542 24LP0542 <24LP0542@G304-CC0227.local>
2025-02-24 12:33:57 +0800

    mongodb

---
+++ src/data-base/mongodb.ts
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+export async function getMovie(year: number, imdb: number) {
+  const uri = process.env.MONGO_URL || "";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+    const findQuery = {
+      $and: [{ "imdb.rating": { $gt: imdb } }, { year: { $gt: year } }],
+    };
+    const movies = await collection.find(findQuery).limit(10).toArray();
+    return movies;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
+}
